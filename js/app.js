efd67c3cb3dcbfe16fabf8df48a254925f4e82df
youmna <youmna_salah@icloud.com>
2017-03-25 21:09:51 +0200

    A hopfully responsive grid

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(document).foundation()
 $(document).ready(function() {
-})
+	var windowHeight = window.innerHeight;
+		var windowWidth = window.innerWidth;
+		// var numRows = document.getElementsByClassName('row').length;
+		var numCols = document.getElementsByClassName('col').length;
+		// var colsPerRows = 1 + windowHeight/300;
+	// alert("hi");
+	 $('.img').css('height', windowHeight/(4) + 'px'); 
+	 // $('.img').css('width', windowHeight/(colsPerRows) + 'px'); 
+	 // $('[class*=\'col\']').css('margin', 0.75*windowWidth*0.0011 + '%'); 
+	 // $('.col').css('width', (100)/(colsPerRows)   + '%'); 
+
+
+});
